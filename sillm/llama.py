Add correct RoPE scaling config

sillm/llama.py
@@ -57,7 +57,12 @@ class Attention(nn.Module):
         self.wv = nn.Linear(args.dim, args.n_kv_heads * args.head_dim, bias=False)
         self.wo = nn.Linear(args.n_heads * args.head_dim, args.dim, bias=False)
 
-        scale = 1 / args.rope_scaling_factor
+        if args.rope_scaling is None:
+            scale = 1
+        elif args.rope_scaling["type"] == "linear":
+            scale = 1 / args.rope_scaling["factor"]
+        else:
+            raise NotImplementedError(f"Unknown scaling type {args.rope_scaling['type']}")
         self.rope = nn.RoPE(args.head_dim,
                             traditional=args.rope_traditional,
                             base=args.rope_theta,