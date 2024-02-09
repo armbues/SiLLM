78e13e13c4a7bd11fc4271e0ab75b08382354e3b
Armin Buescher <armin.buescher@googlemail.com>
2024-02-09 13:12:57 +0100

    Switch to psutil for memory logging

--- sillm/utils.py
+++ sillm/utils.py
@@ -1,29 +1,15 @@
-import os
-import resource
 import logging
 
-def get_process_memory() -> int:
-    """
-    Get process memory usage.
-    Returns:
-        int: process memory usage in bytes
-    """
-    return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-
-def get_system_memory() -> int:
-    """
-    Get total system memory.
-    Returns:
-        int: total system memory in bytes
-    """
-    return os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
+import psutil
     
 def log_memory_usage():
     """
     Log memory usage.
     """
-    process_memory = get_process_memory() // (1024 * 1024)
-    system_memory = get_system_memory() // (1024 * 1024)
-    memory_utilization = 100 * process_memory / float(system_memory)
+    process = psutil.Process()
 
-    logging.debug(f"Max. memory utilization: {process_memory:,} MB / {system_memory:,} MB ({memory_utilization:.2f}%)")
+    process_memory = process.memory_info().rss // (1024 ** 2)
+    system_memory = psutil.virtual_memory().total // (1024 ** 2)
+    memory_utilization = (process_memory / system_memory) * 100
+
+    logging.debug(f"Memory utilization: {process_memory:,} MB / {system_memory:,} MB ({memory_utilization:.2f}%)")
