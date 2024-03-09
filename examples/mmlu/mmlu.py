a5612d74fda537318806a992a256f91f41f3151a
Armin Buescher <armin.buescher@googlemail.com>
2024-03-09 21:13:10 +0100

    Update MMLU example

--- examples/mmlu/mmlu.py
+++ examples/mmlu/mmlu.py
@@ -15,3 +15,11 @@ if __name__ == "__main__":
     model = sillm.load(args.model)
 
     mmlu = datasets.load_dataset("cais/mmlu", "all", split="test")
+
+    for entry in mmlu:
+        prompt = f"Answer the multiple choice question below in the subject {entry['subject'].replace('_', ' ')}.\n\n"
+        prompt += entry["question"] + "\n\n"
+        prompt += "Choices:\n"
+        for i, choice in enumerate(entry["choices"]):
+            prompt += f"{i+1}. {choice}\n"
+        prompt += "\n Answer: "
