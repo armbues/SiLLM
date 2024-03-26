f8a555f063178dff16d9723b05ca5abcdf563835
Armin Buescher <armin.buescher@googlemail.com>
2024-03-26 08:31:10 +0100

    Refactor templates

--- sillm/__init__.py
+++ sillm/__init__.py
@@ -2,7 +2,8 @@ from sillm.core.loader import load
 from sillm.training.dataset import load_dataset
 
 from sillm.core.llm import LLM
-from sillm.core.conversation import Template, AutoTemplate, Conversation, AutoConversation
+from sillm.core.conversation import Conversation, AutoConversation
+from sillm.core.template import Template, AutoTemplate
 from sillm.training.lora import TrainableLoRA
 from sillm.training.dpo import TrainableDPO
 from sillm.training.dataset import DatasetCompletion, DatasetInstruct, DatasetDPO
---
+++ sillm/core/template.py
@@ -0,0 +1,62 @@
+import logging
+
+import jinja2
+import jinja2.exceptions
+
+logger = logging.getLogger("sillm")
+
+default_templates = {
+    "llama": "llama2",
+    "mistral": "mistral",
+    "gemma": "gemma",
+    "mixtral": "mistral",
+    "phi": "phi2",
+    "qwen2": "qwen2"
+}
+
+class Template(object):
+    def __init__(self,
+                 template: str = "chatml",
+                 exception_callback = None
+                 ):
+        loader = jinja2.PackageLoader('sillm', 'templates')
+        env = jinja2.Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
+
+        if exception_callback is None:
+            env.globals["raise_exception"] = self._raise_exception
+        else:
+            env.globals["raise_exception"] = exception_callback
+
+        fname_template = template + ".jinja"
+        self.template = env.get_template(fname_template)
+
+        logger.info(f"Initialized conversation template: {template}")
+
+    @staticmethod
+    def guess_template(args):
+        if args.model_type and args.model_type in default_templates:
+            return default_templates[args.model_type]
+        return None
+
+    def _raise_exception(self, msg):
+        raise jinja2.exceptions.TemplateError(msg)
+
+    def apply_chat_template(self,
+                            messages: list,
+                            add_generation_prompt: bool = False
+                            ):
+        return self.template.render(messages=messages, add_generation_prompt=add_generation_prompt)
+    
+class AutoTemplate(Template):
+    def __init__(self,
+                 tokenizer
+                 ):
+        self.tokenizer = tokenizer
+
+        logger.info(f"Initialized built-in conversation template from tokenizer")
+    
+    def apply_chat_template(self,
+                            messages: list,
+                            add_generation_prompt: bool = False,
+                            ):
+        return self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=add_generation_prompt)
