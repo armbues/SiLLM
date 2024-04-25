648f236a9b2d9a367efb22f5bc0c262a2b7cf121
Armin Buescher <armin.buescher@googlemail.com>
2024-04-25 10:50:17 +0200

    Refactor versioning

--- setup.py
+++ setup.py
@@ -7,9 +7,17 @@ readme = (pkg_dir / "README.md").read_text(encoding="utf-8")
 requirements = (pkg_dir / "requirements.txt").read_text(encoding="utf-8").splitlines()
 requirements_server = (pkg_dir / "requirements-server.txt").read_text(encoding="utf-8").splitlines()
 
+def read_version():
+    with open(pkg_dir / "sillm" / "version.py") as fp:
+        for line in fp:
+            if line.startswith("__version__"):
+                return line.split("=")[1].strip().strip('"')
+            
+    raise ValueError("Version not found")
+
 setup(
     name = "sillm-mlx",
-    version = "0.1.2",
+    version = read_version(),
     description = "Running and training LLMs on Apple Silicon via MLX",
     long_description = readme,
     long_description_content_type = "text/markdown",
--- sillm/__init__.py
+++ sillm/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.3"
+from .version import __version__
 
 from sillm.core.loader import load
 from sillm.training.dataset import load_dataset
