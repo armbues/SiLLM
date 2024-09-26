9459b9d3a951042f849de055ab690ea0f7775741
Armin Buescher <armin.buescher@googlemail.com>
2024-09-26 17:09:45 +0200

    Add relu2

--- sillm/modules/act.py
+++ sillm/modules/act.py
@@ -1,10 +1,17 @@
 import logging
 
+from functools import partial
+
+import mlx.core as mx
 import mlx.nn as nn
 
 from sillm.models.args import ModelArgs
 
 logger = logging.getLogger("sillm")
+
+@partial(mx.compile, shapeless=True)
+def relu2(x):
+    return nn.relu(x).square()
 
 def init_act(args: ModelArgs):
     if args.hidden_act is None:
@@ -12,13 +19,15 @@ def init_act(args: ModelArgs):
         return nn.SiLU()
 
     if args.hidden_act == "silu":
-        return nn.SiLU()
+        return nn.silu
     elif args.hidden_act == "gelu":
-        return nn.GELU()
+        return nn.gelu
     elif args.hidden_act == "gelu_new":
-        return nn.GELU(approx="precise")
+        return nn.gelu
     elif args.hidden_act == "gelu_pytorch_tanh":
-        return nn.GELU()
+        return nn.gelu
+    elif args.hidden_act == "relu2":
+        return relu2
     
     logger.warning(f"Unknown hidden activation: {args.hidden_act}. Using SiLU.")
     return nn.SiLU()
