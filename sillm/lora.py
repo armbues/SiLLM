827bc657c12f26e76a741d68174ff012ac63fbe6
Armin Buescher <armin.buescher@googlemail.com>
2024-10-11 15:51:31 +0200

    Add memory limit option for LoRA training

--- sillm/lora.py
+++ sillm/lora.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output_dir", default=None, type=str, help="Output directory for adapter weights")
     parser.add_argument("-s", "--save_checkpoints", default=False, action="store_true", help="Save model checkpoints to output directory")
     parser.add_argument("-m", "--save_merge", default=None, type=str, help="Save merged model weights to .safetensors file")
-    parser.add_argument("-g", "--grad_checkpoint", default=False, action="store_true", help="Use gradient checkpointing")
     parser.add_argument("--max_length", default=1024, type=int, help="Max token length per training dataset entry (default: 1024)")
     parser.add_argument("--template", type=str, default=None, help="Chat template (chatml, llama-2, alpaca, etc.)")
     parser.add_argument("--layers", default=0, type=int, help="Layers to use for LoRA (default: 0 for all layers)")
@@ -26,6 +25,8 @@ if __name__ == "__main__":
     parser.add_argument("--dropout", default=0.0, type=int, help="Dropout to use for LoRA (default: 0.0)")
     parser.add_argument("--scale", default=10.0, type=float, help="Scale to use for LoRA (default: 10.0)")
     parser.add_argument("--optimizer", type=str, default="adam", help="Optimizer type (default: adam)")
+    parser.add_argument("--grad_checkpoint", default=False, action="store_true", help="Use gradient checkpointing")
+    parser.add_argument("--grad_accu_steps", type=int, default=1, help="Gradient accumulation steps (default: 1)")
     parser.add_argument("--learning_rate", default=1e-5, type=float, help="Learning rate (default: 1e-5)")
     parser.add_argument("--learning_decay", default=0.0, type=float, help="Learning decay for AdamW optimizer (default: 0.0)")
     parser.add_argument("--epochs", default=1, type=int, help="Number of epochs (default: 1)")
@@ -36,6 +37,8 @@ if __name__ == "__main__":
     parser.add_argument("--validation_samples", default=40, type=int, help="Number of validation_samples (default: 40)")
     parser.add_argument("--seed", default=0, type=int, help="Seed for randomization (default: 0)")
     parser.add_argument("--plot", default=None, type=str, help="Create a loss plot and save it to the specified file")
+    parser.add_argument("--memory_limit", default=None, type=float, help="Memory limit for training (default: None)")
+    parser.add_argument("--relax_memory_limit", default=False, action="store_true", help="Relax memory limit for training")
     parser.add_argument("-q4", default=False, action="store_true", help="Quantize the model to 4 bits")
     parser.add_argument("-q8", default=False, action="store_true", help="Quantize the model to 8 bits")
     parser.add_argument("-v", "--verbose", default=1, action="count", help="Increase output verbosity")
@@ -56,6 +59,10 @@ if __name__ == "__main__":
     # Log commandline arguments
     if log_level <= 10:
         utils.log_arguments(args.__dict__)
+
+    # Set memory limit
+    if args.memory_limit is not None:
+        utils.set_memory_limit(args.memory_limit, relaxed=args.relax_memory_limit)
 
     # Load base model
     model = sillm.load(args.model)
@@ -127,16 +134,17 @@ if __name__ == "__main__":
 
         # Model training
         training_config = {
-            "grad_checkpoint":      args.grad_checkpoint,
-            "batch_size":           args.batch_size,
-            "optimizer_type":       args.optimizer,
-            "learning_rate":        args.learning_rate,
-            "learning_decay":       args.learning_decay,
-            "epochs":               args.epochs,
-            "iterations":           args.iterations,
-            "report_steps":         args.report_steps,
-            "eval_steps":           args.eval_steps,
-            "validation_samples":   args.validation_samples,
+            "batch_size":                   args.batch_size,
+            "optimizer_type":               args.optimizer,
+            "learning_rate":                args.learning_rate,
+            "learning_decay":               args.learning_decay,
+            "gradient_checkpointing":       args.grad_checkpoint,
+            "gradient_accumulation_steps":  args.grad_accu_steps,
+            "epochs":                       args.epochs,
+            "iterations":                   args.iterations,
+            "report_steps":                 args.report_steps,
+            "eval_steps":                   args.eval_steps,
+            "validation_samples":           args.validation_samples,
         }
         model.train(dataset_training,
                     dataset_validation,
