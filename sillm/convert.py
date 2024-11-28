9987307a0f7e6fa612e85223dd7e2087b738ae0f
Armin Buescher <armin.buescher@googlemail.com>
2024-11-28 08:36:16 +0100

    Add 3/6 bit quantization to args

--- sillm/convert.py
+++ sillm/convert.py
@@ -10,7 +10,10 @@ if __name__ == "__main__":
     parser.add_argument("output", type=str, help="The output model directory or file")
     parser.add_argument("-a", "--input_adapters", default=None, type=str, help="Load LoRA adapter weights from .safetensors file")
     parser.add_argument("-r", "--remap", default=False, action="store_true", help="Remap weights keys to native SiLLM format")
+    parser.add_argument("-q2", default=False, action="store_true", help="Quantize the model to 2 bits")
+    parser.add_argument("-q3", default=False, action="store_true", help="Quantize the model to 3 bits")
     parser.add_argument("-q4", default=False, action="store_true", help="Quantize the model to 4 bits")
+    parser.add_argument("-q6", default=False, action="store_true", help="Quantize the model to 6 bits")
     parser.add_argument("-q8", default=False, action="store_true", help="Quantize the model to 8 bits")
     parser.add_argument("-dtype", type=str, default=None, help="Cast model weights to specified data type")
     parser.add_argument("-m", "--max_shard_size", type=int, default=5368709120, help="Maximum shard size in bytes")
@@ -42,8 +45,14 @@ if __name__ == "__main__":
         model.merge_and_unload_lora()
 
     # Quantize model or cast model weights
-    if args.q4 is True:
+    if args.q2 is True:
+        model.quantize(bits=2)
+    elif args.q3 is True:
+        model.quantize(bits=3)
+    elif args.q4 is True:
         model.quantize(bits=4)
+    elif args.q6 is True:
+        model.quantize(bits=6)
     elif args.q8 is True:
         model.quantize(bits=8)
     elif args.dtype is not None:
