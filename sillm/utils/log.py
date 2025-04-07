e310a09df7aee7084d2eab9ca6dac17a9cc30d09
Armin Buescher <armin.buescher@googlemail.com>
2025-04-07 10:45:44 +0200

    Refactor mx.metal calls

--- sillm/utils/log.py
+++ sillm/utils/log.py
@@ -45,11 +45,11 @@ def log_arguments(args):
         logger.debug(f"Argument: {key} = {value}")
 
 def log_memory_usage(reset=False):
-    peak_memory = mx.metal.get_peak_memory()
+    peak_memory = mx.get_peak_memory()
     system_memory = os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES")
     memory_usage = peak_memory / system_memory
 
     logger.debug(f"Peak memory: {(peak_memory // (1024 ** 2)):,} MB ({memory_usage:.2%} of system memory)")
 
     if reset:
-        mx.metal.reset_peak_memory()
+        mx.reset_peak_memory()
--- sillm/utils/metal.py
+++ sillm/utils/metal.py
@@ -17,4 +17,4 @@ def set_memory_limit(memory_limit: float = 0.9,
     system_memory = os.sysconf("SC_PAGE_SIZE") * os.sysconf("SC_PHYS_PAGES")
 
     logger.info(f"Setting memory limit to {memory_limit:.0%} of system memory")
-    mx.metal.set_memory_limit(int(memory_limit * system_memory), relaxed=relaxed)
+    mx.set_memory_limit(int(memory_limit * system_memory), relaxed=relaxed)
